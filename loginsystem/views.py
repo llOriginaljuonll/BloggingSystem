8efbe14e880d0be2f51576ae0e89909f92aa0801
Originaljuon lastsecond1989@gmail.com <jisoooh0202@gmail.com>
2023-10-16 16:45:43 +0700

    50-28

--- loginsystem/views.py
+++ loginsystem/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def index(request):
@@ -17,7 +17,14 @@ def register(request):
             return redirect('member')
         else:
             if password == repassword :
-                user = User.objects.create_user(
+                if User.objects.filter(username=username).exists():
+                      messages.info(request, 'Username นี้มีคนใช้แล้ว')
+                      return redirect('member')
+                elif User.objects.filter(email=email).exists():
+                      messages.info(request, 'Email นี้เคยลงทะเบียนไปแล้ว')
+                      return redirect('member')
+                else:
+                    user = User.objects.create_user(
                     username = username,
                     email = email,
                     password = password
@@ -28,3 +35,20 @@ def register(request):
             else:
                 messages.info(request, 'ไม่สามารถลงทะเบียนได้รหัสผ่านไม่ตรงกัน')
                 return redirect('member')
+            
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    
+    if user is not None:
+        auth.login(request, user)
+        return redirect('panel')
+    else:
+        messages.info(request, 'ไม่พบข้อมูลบัญชีผู้ใช้')
+        return redirect('member')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('member')
+	
--- writerpanel/views.py
+++ writerpanel/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blogs.models import Blogs
 from django.db.models import Sum
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import auth
+from category.models import Category
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+@login_required(login_url='member')
 def panel(request):
-    writer = 'jojo'
+    writer = auth.get_user(request)
     blogs = Blogs.objects.filter(writer=writer)
     blogCount = blogs.count()
     total = Blogs.objects.filter(writer=writer).aggregate(Sum('views'))
     return render(request, 'backend/index.html', {'blogs':blogs, 'writer':writer, 'blogCount':blogCount, 'total':total})
+
+def displayForm(request):
+    writer = auth.get_user(request)
+    blogs = Blogs.objects.filter(writer=writer)
+    blogCount = blogs.count()
+    total = Blogs.objects.filter(writer=writer).aggregate(Sum('views'))
+    categories = Category.objects.all()
+    return render(request, 'backend/blogForm.html', {'blogs':blogs, 'writer':writer, 'blogCount':blogCount, 'total':total, 'categories':categories})
+    
+def insertData(request):
+	if request.method == 'POST' and request.FILES['image']:
+          image = request.FILES['image']
+          fs = FileSystemStorage()
+          #อัพโหลด
+          img_url = 'blogImages/' + image.name
+          filename = fs.save(img_url + image.name, image)
+          return redirect('displayForm')
